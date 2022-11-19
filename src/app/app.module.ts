d810a668dca9900752ef6660587bffcbd210ba0e
Rôney Fischer <fischer.roney@gmail.com>
2022-11-18 22:52:24 -0300

    same

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
@@ -15,6 +15,8 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { PassagemDadosComponent } from './components/passagem-dados/passagem-dados.component';
 import { PipeComponent } from './components/pipe/pipe.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+
 
 @NgModule({
   declarations: [
@@ -31,12 +33,14 @@ import { PipeComponent } from './components/pipe/pipe.component';
           LoginFormComponent,
           ListRenderComponent,
           PassagemDadosComponent,
-          PipeComponent
+          PipeComponent,
+          TwoWayBindingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
